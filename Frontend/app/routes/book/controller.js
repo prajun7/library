3ab0479d0f3e5d3e5ee7a4dacbb891c3dbec8d76
Prajun Trital <prajuncs@gmail.com>
2023-04-15 17:59:36 -0500

    clean up

--- Frontend/app/routes/book/controller.js
+++
@@ -1,6 +0,0 @@
-import Controller from "@ember/controller";
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
-
-export default class BookController extends Controller {
-}
