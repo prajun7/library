0a07fecd8eaf6c155d0569eae38cba03b1e39364
Prajun Trital <prajuncs@gmail.com>
2023-04-15 22:31:30 -0500

    partially working fetch

--- Frontend/app/routes/index.js
+++ Frontend/app/routes/index.js
@@ -1,17 +1,18 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 // Route files are like useEffect() from React,
 // Whenever we go to this page it automatically runs the mode only once and get the
 // data from the backend.
 // Just like useEffect with empty array, []
 
-export default class IndexRoute extends Route {
+export default class IndexRoute extends Route { 
   model() {
-    console.log("we are in the route");
-    // return fetch("/books")
-    //     .then(res => res.json())
-    //     .then(res => res.map(o => new Book(o)))
-    //     .then(books => A(books));
-    // }
+    return fetch('http://localhost:9090/books')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
