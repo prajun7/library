7ddde2b0f566d9bf13bba3cc3975dc0f0a12f253
Prajun Trital <prajuncs@gmail.com>
2023-04-16 23:46:15 -0500

    more clean ups

--- Frontend/app/components/confirm-modal.js
+++ Frontend/app/components/confirm-modal.js
@@ -1,9 +1,18 @@
+/*
+We are pasing title, mesage, confirm function and cancel function from the 
+parent component.
+We can get those passed filed using, this.args.onConfirm() to call that method,
+or
+this.args.message to get the messag that we passed from the parent component.
+**/
+
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class MyModalComponent extends Component {
   @action
   confirm() {
+    // console.log(this.args.message);  // logs the message
     this.args.onConfirm();
   }
 
