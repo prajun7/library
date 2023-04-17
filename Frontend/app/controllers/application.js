f2a854416b431603a07acb4828cbbfbab0c9ef10
Prajun Trital <prajuncs@gmail.com>
2023-04-16 23:00:42 -0500

    using h2 database

--- Frontend/app/controllers/application.js
+++ Frontend/app/controllers/application.js
@@ -1,2 +1,8 @@
+/*
+We can use the controller class to add the action that we want to happen
+when we click some button in the template file.
+The name of the controller file, route file and template file should match.
+**/
+
 import Controller from '@ember/controller';
 export default class ApplicationComponent extends Controller{}
