ca87a410cfdafd05ca9a5fdadaaf6efee0a4c6c7
Prajun Trital <prajuncs@gmail.com>
2023-04-20 23:38:20 -0500

    Add dist/ files for GitHub Pages deployment

--- Frontend/app/helpers/ex.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-function ex([start, end, string]) {
-  if(start && end && string ) {
-    return string.substring(start,end)
-  } 
-}
-export default helper(ex);
