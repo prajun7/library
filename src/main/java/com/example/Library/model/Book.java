09f70c9ad451a9af5f861cc7020b2b4c793a8717
Prajun Trital <prajuncs@gmail.com>
2023-04-22 18:48:24 -0500

    Add files via upload

---
+++ src/main/java/com/example/Library/model/Book.java
@@ -0,0 +1,53 @@
+package com.example.Library.model;
+
+import jakarta.persistence.*;
+
+// Option + Command + A
+// https://stackoverflow.com/questions/65643300/why-are-my-classes-marked-in-red-in-intellij-idea?fbclid=IwAR0i0YmE7hpgOPUVAXPX-QExl4QTt9o_7WuQURGvOldkEPbkOSV--DXTxkY#:~:text=It%20means%20that%20the%20files,to%20the%20VCS%20if%20desired
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
