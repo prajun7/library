d8c6d70a498ed961dbd1c175a90d8cb1b4c4107d
Prajun Trital <prajuncs@gmail.com>
2023-04-22 20:42:00 -0500

    clean up

--- Frontend/app/controllers/index.js
+++ Frontend/app/controllers/index.js
@@ -28,12 +28,9 @@ export default class IndexComponent extends Controller{
     .then((res) => res.json())
     .then((res) => {
       const newBook = new Book(res);
-      console.log("this.model in Controller in index.js")
       if (!this.model.books) {
-        console.log("undefined");
         this.model.books = A();
       }
-      console.log(this);
       this.model.books.pushObject(newBook); 
       // Push the new book to this.model array
       // Since in the index.js router file we are returning arrays of data from models,
